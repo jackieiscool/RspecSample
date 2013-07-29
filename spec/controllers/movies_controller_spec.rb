6de5771c63bbab581391e6e259a69df9ffa0ba08
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-07-29 08:45:41 -0700

    remove mocks

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe MoviesController do
 
   before :each do
-    Movie.stub(:get_info).and_return(OMDB_INFO)
+    
   end
 
   describe "GET index" do
     it "assigns movie info to @movies" do
-      get :index
-      assigns(:movies).should eq(OMDB_INFO)
+      
     end
   end
 
